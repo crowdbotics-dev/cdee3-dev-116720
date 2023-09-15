918a62cf7a36865792217da5b500e82d5b81ca51
Crowdbotics <engineering@crowdbotics.com>
2023-09-15 06:32:59 +0000

    Update store.

--- store/testerorg/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios"
-import { TESTERORG_USERNAME, TESTERORG_PASSWORD } from "react-native-dotenv"
-const testerorg = axios.create({
-  baseURL: "https://Vcbdn.com",
-  auth: { username: TESTERORG_USERNAME, password: TESTERORG_PASSWORD },
-  headers: { Accept: "application/json", "Content-Type": "application/json" }
-})
-export const apiService = {}
